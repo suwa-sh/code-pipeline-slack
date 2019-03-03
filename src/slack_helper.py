9ee68eb016d311758b2bd9006ac2c3547d921689
Shinichi Suwa <suwash01@gmail.com>
2019-03-03 15:07:15 +0900

    test(*): 動作確認の整備

--- src/slack_helper.py
+++ src/slack_helper.py
@@ -14,8 +14,6 @@ SLACK_BOT_NAME = os.getenv("SLACK_BOT_NAME", "BuildBot")
 SLACK_BOT_ICON = os.getenv("SLACK_BOT_ICON", ":robot_face:")
 
 CHANNEL_CACHE = {}
-# キャッシュせずに、つどslackのメッセージから対象を取得
-# MSG_CACHE = {}
 
 
 def find_channel(name):
@@ -36,13 +34,13 @@ def find_channel(name):
 
 
 def find_msg(ch):
-#    logger.debug("channels.history channel={}".format(ch))
     r = sc.api_call('channels.history', channel=ch)
     if 'error' in r:
         logger.error("{} channels.history error: {}".format(__name__, r['error']))
         return None
 
     return r
+
 
 def find_my_messages(ch_name, user_name=SLACK_BOT_NAME):
     ch_id = find_channel(ch_name)
@@ -53,14 +51,9 @@ def find_my_messages(ch_name, user_name=SLACK_BOT_NAME):
 
 
 def find_message_for_build(buildInfo):
-#    cached = MSG_CACHE.get(buildInfo.executionId)
-#    if cached:
-#        return cached
-
     for m in find_my_messages(SLACK_CHANNEL):
         for att in msg_attachments(m):
             if att.get('footer') == buildInfo.executionId:
-#                MSG_CACHE[buildInfo.executionId] = m
                 return m
     return None
 
@@ -76,58 +69,62 @@ def msg_fields(m):
 
 
 def post_build_msg(msgBuilder):
-    #  logger.info("-- post_build_msg")
-    #  logger.debug("---- MSG_CACHE: {}".format(json.dumps(MSG_CACHE, default=str)))
-    #  logger.debug("---- CHANNEL_CACHE: {}".format(json.dumps(CHANNEL_CACHE, default=str)))
+    if msgBuilder.isClear:
+        ch_id = find_channel(SLACK_CHANNEL)
+        res = clear_msg(ch_id, msgBuilder.messageId)
+        if res['ok']:
+            res['message']['ts'] = res['ts']
+        return res
 
     if msgBuilder.messageId:
         ch_id = find_channel(SLACK_CHANNEL)
         msg = msgBuilder.message()
-        r = update_msg(ch_id, msgBuilder.messageId, msg)
-        #    logger.debug("---- response: {}".format(json.dumps(r, default=str)))
-        if r['ok']:
-            r['message']['ts'] = r['ts']
-#            MSG_CACHE[msgBuilder.buildInfo.executionId] = r['message']
-        return r
+        res = update_msg(ch_id, msgBuilder.messageId, msg)
+        if res['ok']:
+            res['message']['ts'] = res['ts']
+        return res
 
-    r = send_msg(SLACK_CHANNEL, msgBuilder.message())
-    if r['ok']:
-         # logger.debug("---- response: {}".format(json.dumps(r, default=str)))
-#        MSG_CACHE[msgBuilder.buildInfo.executionId] = r['message']
-        CHANNEL_CACHE[SLACK_CHANNEL] = r['channel']
+    res = send_msg(SLACK_CHANNEL, msgBuilder.message())
+    if res['ok']:
+        CHANNEL_CACHE[SLACK_CHANNEL] = res['channel']
 
-    return r
+    return res
 
 
 def send_msg(ch, attachments):
-#    logger.info("---- send_msg")
-#    logger.info("------ attachments: {}".format(attachments))
-    r = sc_bot.api_call(
+    res = sc_bot.api_call(
         "chat.postMessage",
         channel=ch,
         icon_emoji=SLACK_BOT_ICON,
         username=SLACK_BOT_NAME,
         attachments=attachments
-        )
-#    logger.info("------ res: {}".format(json.dumps(r)))
-    return r
+    )
+    return res
 
 
 def update_msg(ch, ts, attachments):
-#    logger.info("---- update_msg")
-#    logger.info("------ attachments: {}".format(attachments))
-    r = sc_bot.api_call(
+    res = sc_bot.api_call(
         'chat.update',
         channel=ch,
         ts=ts,
         icon_emoji=SLACK_BOT_ICON,
         username=SLACK_BOT_NAME,
         attachments=attachments
-#        attachments=[{
-#            'color': 'good',
-#            'fields': [{'short': True, 'value': 'TEST', 'title': 'twooca-CD-sandbox'}],
-#            'footer': 'd8d56070-9a62-480c-90fa-07bc2975c2c1'
-#        }]
-        )
-#    logger.info("------ res: {}".format(json.dumps(r)))
-    return r
+    )
+    return res
+
+
+def clear_msg(ch, ts):
+    res = sc_bot.api_call(
+        'chat.update',
+        channel=ch,
+        ts=ts,
+        icon_emoji=SLACK_BOT_ICON,
+        username=SLACK_BOT_NAME,
+        attachments=[{
+            'color': 'good',
+            'fields': [{'short': True, 'value': 'TEST', 'title': 'twooca-CD-sandbox'}],
+            'footer': 'd8d56070-9a62-480c-90fa-07bc2975c2c1'
+        }]
+    )
+    return res
